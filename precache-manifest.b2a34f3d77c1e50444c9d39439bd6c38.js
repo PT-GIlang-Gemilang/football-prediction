866215ce1f43690969ffc16c72040c4a70066699
mgilang13 <muhammad.gilang.n.k@gmail.com>
2019-08-30 10:53:21 +0700

    Updates

---
+++ precache-manifest.b2a34f3d77c1e50444c9d39439bd6c38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de54694ef6db7d908fefb46af9d3e38e",
+    "url": "/recipeappreact/index.html"
+  },
+  {
+    "revision": "e5c3da59b51ee7b68652",
+    "url": "/recipeappreact/static/css/main.71e8b785.chunk.css"
+  },
+  {
+    "revision": "4bf5643fd0716f388036",
+    "url": "/recipeappreact/static/js/2.824f1f25.chunk.js"
+  },
+  {
+    "revision": "e5c3da59b51ee7b68652",
+    "url": "/recipeappreact/static/js/main.f290b139.chunk.js"
+  },
+  {
+    "revision": "6724df74e832705919d0",
+    "url": "/recipeappreact/static/js/runtime~main.474ae16a.js"
+  }
+]);
